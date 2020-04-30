384efb8ee7b85fd7de2cddad3683286624cbb0eb
ross <clear>
2020-04-30 01:19:31 -0500

    Se agregaron las activity de login, registro de académico, bienvenida a usuario

---
+++ app/src/main/java/com/example/mykeepapp/RegistroAcademico.kt
@@ -0,0 +1,12 @@
+package com.example.mykeepapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroAcademico : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_academico)
+    }
+}
