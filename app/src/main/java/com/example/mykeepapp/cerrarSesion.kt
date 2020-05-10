fd254c00aae348b68035d465c90a1d8859044aab
ross <clear>
2020-05-10 17:18:42 -0500

    se agregan dos activity, el Acuerdo de confidencialidad y el Logout

---
+++ app/src/main/java/com/example/mykeepapp/cerrarSesion.kt
@@ -0,0 +1,12 @@
+package com.example.mykeepapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cerrarSesion : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cerrar_sesion)
+    }
+}
